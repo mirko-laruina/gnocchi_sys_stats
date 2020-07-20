1d5a46518c2d27e57b908f7665c40ecb17bb1844
Mirko <mirkoletto@gmail.com>
2020-07-20 15:36:07 +0200

    Producer: verbose output and better uuid managem

--- producer/producer.py
+++ producer/producer.py
@@ -2,17 +2,18 @@
 
 import argparse
 import psutil
-from time import sleep, time
-import requests
+from time import sleep
 import os, sys
 import gnocchi_api
 
+verbose = False
+
 def get_machine_uuid():
     try:
-        f = open('uuid', 'r')
-        return f.read().split('\n')[0]
+        f = open('uuid', 'r') if os.path.isfile('uuid') else os.popen('uuidgen | tee uuid')
     except:
-        return os.popen('uuidgen | tee uuid').read().split('\n')[0]
+        print("Error retrieving UUID")
+    return f.read().split('\n')[0]
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Gnocchi producer for system usage informations")
@@ -20,8 +21,10 @@ if __name__ == '__main__':
                         help="Amount of seconds between measurements")
     parser.add_argument("-t", "--token",  type=str, required=True, help="Authentication token")
     parser.add_argument("-u", "--url", type=str, default='http://252.3.47.9:8041/', help="Gnocchi listening URL")
+    parser.add_argument("-v", "--verbose", action="store_true", help="Show verbose output")
     args = parser.parse_args()
 
+    verbose = args.verbose
     uuid = get_machine_uuid()
     print("Machine UUID: %s"%uuid)
 
@@ -42,9 +45,15 @@ if __name__ == '__main__':
 
     while True:
         try:
-            gnocchi.send_measure(metrics['cpu'], psutil.cpu_percent())
+            cpu_util = psutil.cpu_percent()
+            mem_util = psutil.virtual_memory()[3]
+            gnocchi.send_measure(metrics['cpu'], cpu_util)
             # virtual_memory()[3] returns the currently used memory
-            gnocchi.send_measure(metrics['memory'], psutil.virtual_memory()[3])
+            gnocchi.send_measure(metrics['memory'], mem_util)
+
+            if args.verbose:
+                print("Pushed new measurement. CPU: %f%% Memory: %d"%(cpu_util, mem_util))
+                
         except gnocchi_api.AuthException:
             print("Authentication token has expired")
             sys.exit(1)
