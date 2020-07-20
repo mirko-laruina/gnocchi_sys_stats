4d919fcc3b43f279a0607cb02719c0d2ebe5b4b5
Mirko <mirkoletto@gmail.com>
2020-07-20 14:00:44 +0200

    Producer: code splitted in two files

---
+++ producer/gnocchi_api.py
@@ -0,0 +1,64 @@
+from time import time
+import requests
+
+class AuthException(Exception):
+    ''' Raised when the auth token has expired '''
+    pass
+
+class GnocchiAPI:
+    def __init__(self, url, token):
+        self.headers = { 
+            'X-AUTH-TOKEN': token,
+            'Content-Type': 'application/json'
+        }
+
+        self.base_url = url
+
+    def get_metric(self):
+        r = requests.get(self.base_url + "/v1/metric", headers=self.headers)
+        if r.status_code == 401:
+            raise AuthException()
+        elif r.status_code // 100 != 2:
+            raise Exception('Response status code: %d'%r.status_code)
+        
+        return r.json()[-1]['id'].encode('ascii', 'ignore')
+
+    def get_metrics_from_resource(self, resource_id):
+        r = requests.get(self.base_url + '/v1/resource/generic/' + resource_id, headers=self.headers)
+        if r.status_code == 404:
+            # we need to create the resource
+            r = requests.post(self.base_url + '/v1/resource/generic', headers=self.headers, json={
+                    "id": resource_id,
+                    "project_id": '3d22f640-84db-40ce-af49-44468443234f',
+                    "user_id": '3d22f640-84db-40ce-af49-44468443234f',
+                    "metrics": {
+                        "cpu": {
+                            "archive_policy_name": "high"
+                        },
+                        "memory": {
+                            "archive_policy_name": "high"
+                        }
+                    }
+                } )
+        elif r.status_code == 401:
+            raise AuthException()
+        elif r.status_code // 100 != 2:
+            raise Exception('Response status code: %d'%r.status_code)
+
+        return r.json()['metrics']
+
+    def send_measure(self, metric_id, value, time = time()):
+        # Gnocchi returns ISO 8601 timestamps, but accepts different formats. Unix epoch is OK
+        r = requests.post(
+            self.base_url+"/v1/metric/"+metric_id+"/measures",
+            headers=self.headers,
+            json=[{
+                "timestamp": time,
+                "value": value
+                }]
+            )
+        
+        if r.status_code == 401:
+            raise AuthException()
+        elif r.status_code // 100 != 2:
+            raise Exception('Response status code: %d'%r.status_code)
---
+++ producer/producer.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+
+import argparse
+import psutil
+from time import sleep, time
+import requests
+import os, sys
+import gnocchi_api
+
+def get_machine_uuid():
+    try:
+        f = open('uuid', 'r')
+        return f.read().split('\n')[0]
+    except:
+        return os.popen('uuidgen | tee uuid').read().split('\n')[0]
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Gnocchi producer for system usage informations")
+    parser.add_argument("-i", "--interval", type=float, default=60,
+                        help="Amount of seconds between measurements")
+    parser.add_argument("-t", "--token",  type=str, required=True, help="Authentication token")
+    parser.add_argument("-u", "--url", type=str, default='http://252.3.47.9:8041/', help="Gnocchi listening URL")
+    args = parser.parse_args()
+
+    uuid = get_machine_uuid()
+    print("Machine UUID: %s"%uuid)
+
+    gnocchi = gnocchi_api.GnocchiAPI(args.url, args.token)
+
+    try:
+        metrics = gnocchi.get_metrics_from_resource(uuid)
+        print("Metrics: %s"%metrics)
+    except gnocchi_api.AuthException:
+        print("Authentication token has expired")
+        sys.exit(1)
+    except Exception as e:
+        print("Application exception")
+        print(e)
+        sys.exit(1)
+
+        
+
+    while True:
+        try:
+            gnocchi.send_measure(metrics['cpu'], psutil.cpu_percent())
+            # virtual_memory()[3] returns the currently used memory
+            gnocchi.send_measure(metrics['memory'], psutil.virtual_memory()[3])
+        except gnocchi_api.AuthException:
+            print("Authentication token has expired")
+            sys.exit(1)
+        except Exception as e:
+            print("Application exception")
+            print(e)
+            sys.exit(1)
+
+        sleep(args.interval)
